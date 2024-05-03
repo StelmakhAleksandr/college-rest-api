63d09d2487f8d305ad749c804a01f59a8fa32137
Oleksandr Stelmakh <oleksandr.stelmakh@obreey-products.com>
2024-05-03 21:41:39 +0300

    students

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+
+
+$dbSettings = [
+    'host' => '185.253.218.168',
+    'dbname' => 'edpmaold_college',
+    'user' => 'edpmaold_college',
+    'pass' => 'qwerty123'
+];
+
+$pdo = new PDO("mysql:host={$dbSettings['host']};dbname={$dbSettings['dbname']};charset=utf8", $dbSettings['user'], $dbSettings['pass']);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// Тестовий маршрут
+$app->get('/', function ($request, $response, $args) {
+    $response->getBody()->write("Hello, world!");
+    return $response;
+});
+
+// $app->get('/students', function (Request $request, Response $response) use ($pdo) {
+//     $stmt = $pdo->query('SELECT * FROM students');
+//     $students = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//     $response->getBody()->write(json_encode($students));
+//     return $response->withHeader('Content-Type', 'application/json');
+// });
+
+$app->get('/students', function (Request $request, Response $response, array $args) use ($pdo) {
+    $sql = "SELECT 
+                People.firstName,
+                People.lastName,
+                People.phoneNumber,
+                People.email,
+                Groups.groupName
+            FROM 
+                Students
+            JOIN 
+                People ON Students.personId = People.personId
+            JOIN 
+                Groups ON Students.groupId = Groups.groupId";
+                
+    $stmt = $pdo->query($sql);
+    $students = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    $response->getBody()->write(json_encode($students));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+
+$app->run();
